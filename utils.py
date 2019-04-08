81a9993cdb68df13f2ee97c5cef1cbc5c8124c28
ovoNiku <2891247632@qq.com>
2019-04-08 22:07:30 +0800

    tool

--- utils.py
+++ utils.py
@@ -2,8 +2,6 @@ import time
 
 
 def log(*args, **kwargs):
-    # time.time() 返回 unix time
-    # 如何把 unix time 转换为普通人类可以看懂的格式呢？
     time_format = '%Y/%m/%d %H:%M:%S'
     localtime = time.localtime(int(time.time()))
     formatted = time.strftime(time_format, localtime)
